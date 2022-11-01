c35a12414d27794db84f4130b486848f11041227
Eoin Motherway <25342760+YuKitsune@users.noreply.github.com>
2022-11-01 11:16:02 +1000

    Back to OneDark

--- .hyper.js
+++ .hyper.js
@@ -130,7 +130,7 @@ module.exports = {
         // set to true to preserve working directory when creating splits or tabs
         preserveCWD: true,
         // for advanced config flags please refer to https://hyper.is/#cfg
-        "verminal": {
+        "hyper-one-dark": {
             fontFamily: 'MesloLGS NF',
         }
     },
@@ -140,7 +140,7 @@ module.exports = {
     //   `hyperpower`
     //   `@company/project`
     //   `project#1.0.1`
-    plugins: ["verminal"],
+    plugins: ["hyper-one-dark"],
     // in development, you can create a directory under
     // `~/.hyper_plugins/local/` and include it here
     // to load it and avoid it being `npm install`ed
