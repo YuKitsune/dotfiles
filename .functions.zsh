db1e6c6331073b14a723a4eaa64291968435440f
Eoin Motherway <25342760+YuKitsune@users.noreply.github.com>
2022-12-20 10:08:30 +1000

    Add some functions

--- .functions.zsh
+++ .functions.zsh
@@ -39,6 +39,18 @@ check-dotfile-updates() {
     popd > /dev/null
 }
 
+# Reads a value from a .env file
+env() {
+
+	if [ -z "$1" ] || [ -z "$2" ]; then
+		echo "Usage:"
+		echo "	$0 <path_to_env_file> <variable_name>"
+		return 1
+	fi
+
+	cat $1 | grep $2 | head -1 | awk -F'=' '{print $2}'
+}
+
 # Git
 ## Quick commit: 
 ## 1. Stage all changes
@@ -58,6 +70,24 @@ qc() {
     else
 	gum spin --title="Pushing" --show-output -- git push -f
     fi
+}
+
+## Prints the currently checked out git ref (Branch, tag, or commit hash)
+ref() {
+	branch_name=$(git symbolic-ref -q --short HEAD 2> /dev/null)
+	if [ $? -eq 0 ]; then
+		echo $branch_name
+		return 0
+	fi
+
+	tag_name=$(git describe --tags --exact-match 2> /dev/null)
+	if [ $? -eq 0 ]; then
+		echo $tag_name
+		return 0
+	fi
+
+	hash=$(git rev-parse HEAD)
+	echo $hash
 }
 
 # Kubectl
