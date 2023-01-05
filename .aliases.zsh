a3ecf388950e360c9acf41ef1d152c1e2681014b
Eoin Motherway <25342760+YuKitsune@users.noreply.github.com>
2023-01-06 09:18:54 +1000

    Add additional aliases

--- .aliases.zsh
+++ .aliases.zsh
@@ -7,7 +7,7 @@ alias flushdns="sudo dscacheutil -flushcache; sudo killall -HUP mDNSResponder"
 alias l="ls -lah --color"
 alias diff="diff --color"
 
-# nvims
+# nvim
 alias vi="nvim"
 alias vim="nvim"
 
@@ -25,15 +25,22 @@ alias kpv="kubectl get pv"
 alias kpvc="kubectl get pvc"
 
 # docker
+alias d="docker"
 alias dp="docker ps"
-alias ds="docker start"
 alias dq="docker ps --format \"{{.Names}}\" | gum choose --no-limit | xargs docker stop"
 alias ld="lazydocker"
 
 # git
 alias g="git"
+alias gl="git log"
+alias gs="git status"
+
 alias gc="git checkout"
 alias ga="git add ."
 alias gcm="git commit -m"
+alias gd="git pull" # "pull Down"
+alias gu="git push" # "push Up"
+
 alias yeet="git add . && git commit -m 'yeet' && git push"
+
 alias lg="lazygit"
