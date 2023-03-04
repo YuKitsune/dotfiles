b0ec70b70e3d9e50c0d241a8959d490af28d646a
Eoin Motherway <25342760+YuKitsune@users.noreply.github.com>
2023-03-04 16:43:17 +1000

    Remove auto-update

--- shell/zsh/.functions.zsh
+++ shell/zsh/.functions.zsh
@@ -1,44 +1,4 @@
 
-# Todo: Move this somewhere else so we don't mess up the other functions
-check-dotfile-updates() {
-    pushd $HOME/Developer/github.com/yukitsune/dotfiles > /dev/null
-
-	temp_file=$(mktemp)
-	gum spin --title="Checking for updated to dotfiles..." -- git fetch --dry-run > $temp_file
-    output=$(cat $temp_file)
-
-    if [ -n "$output" ]
-    then
-        gum confirm "🆕 Updated dotfiles available! Do you want to clone and update now?"
-        if [ $? -ne 0 ]
-        then
-            return 0
-        fi
-
-        git pull
-        if [ $? -ne 0 ]
-        then
-            echo "❌ Failed to pull dotfiles"
-            return 1
-        fi
-
-        sh ./dotfiles.sh sync
-        if [ $? -ne 0 ]
-        then
-            echo "❌ Failed to sync dotfiles"
-            return 1
-        fi
-
-        gum confirm "✅ Dotfiles updated and synced! Do you want to restart your terminal now?"
-        if [ $? -eq 0 ]
-        then
-            exit 0
-        fi
-    fi
-
-    popd > /dev/null
-}
-
 # Reads a value from a .env file
 env() {
 
