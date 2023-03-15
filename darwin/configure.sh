60a5623b730918746ba894841a3eb700863ff3e3
Eoin Motherway <25342760+YuKitsune@users.noreply.github.com>
2023-03-15 18:29:35 +1000

    Revert "Try Firefox"
    This reverts commit 387ace0a25910fd328c593e81275577e0680424d.

--- darwin/configure.sh
+++ darwin/configure.sh
@@ -131,7 +131,6 @@ function configure_macos() {
     write_default com.apple.assistant.support "Assistant Enabled" -bool false
 
     # Search engine
-    # Todo: This doesn't seem to work...
     echo "📝 [.GlobalPreferences] \"NSPreferredWebServices\":\"NSWebServicesProviderWebSearch\":\"NSDefaultDisplayName\" DuckDuckGo"
     /usr/libexec/PlistBuddy -c "Set :\"NSPreferredWebServices\":\"NSWebServicesProviderWebSearch\":\"NSDefaultDisplayName\" DuckDuckGo" $HOME/Library/Preferences/.GlobalPreferences.plist
 
@@ -258,7 +257,7 @@ function add_dock_item() {
     path=$1
     echo "📝 Adding $path to dock" > /dev/tty
 
-    dockutil --add "$path" --no-restart > /dev/null
+    dockutil --add $path --no-restart > /dev/null
     print_result_if_failed $? "Failed to set dock item"
 }
 
@@ -284,7 +283,7 @@ function configure_dock() {
     # Setup the dock icons
     echo "📝 Clearing dock" > /dev/tty
     dockutil --remove all --no-restart
-    add_dock_item "/Applications/Firefox Developer Edition.app"
+    add_dock_item /Applications/Safari.app
     add_dock_item /System/Applications/Mail.app
     add_dock_item /System/Applications/Calendar.app
     add_dock_item /System/Applications/Notes.app
