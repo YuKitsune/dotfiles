5a1692e6af0b555cbbfe0fa9a3c98771c82f6b42
Eoin Motherway <25342760+YuKitsune@users.noreply.github.com>
2026-08-24 16:18:49 +1000

    Bump brews

--- darwin/Brewfile.work.rb
+++ darwin/Brewfile.work.rb
@@ -1,8 +1,17 @@
 tap "octopusdeploy/taps" 
+tap "hashicorp/tap"
 brew "octopusdeploy/taps/octopus-cli"
 cask "1password-cli"
-cask "powershell"
+# Command-line shell and scripting language
+brew "powershell", link: false
 cask "slack"
 cask "meetingbar"
 cask "docker-desktop"
+# Desktop virtualization software
+cask "parallels"
 vscode "octopusdeploy.vscode-octopusdeploy"
+vscode "hashicorp.hcl"
+
+# Terraform
+brew "hashicorp/tap/terraform", trusted: true
+
